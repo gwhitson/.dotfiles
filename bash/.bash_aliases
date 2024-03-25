44a5e7398a992bb1cc259971deeb3974a6a30433
gavin <rockywhitson4@gmail.com>
2024-03-25 14:42:00 -0500

    added full-setup script as one file for easier running on fresh install, specific to ubuntu right now -- sue me

--- bash/.bash_aliases
+++ bash/.bash_aliases
@@ -1,3 +1,4 @@
 alias sh='bash'
 alias vi='nvim'
 alias vim='nvim'
+alias clear_nvim_cache='rm ~/.local/nvim/state/swap/*'
---
+++ scripts/ubu-setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+sudo apt update -y
+sudo apt install neofetch -y
+sudo apt install gcc -y
+sudo apt install git -y
+sudo apt install python3.10-venv -y
+sudo apt install imagemagick -y
+sudo apt install python3-pip -y
+sudo apt install tmux -y
+sudo pip install pywal
+sudo snap install nvim --classic
+
+
+git clone https://github.com/gwhitson/.dotfiles ~/.config/.dotfile
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+rm -rf ~/.config/tmux
+rm -rf ~/.config/nvim
+rm -rf ~/.cache/wal
+sudo rm /bin/editor
+rm -rf ~/.bashrc
+rm -rf ~/.bash_aliases
+
+ln -s ~/.config/.dotfiles/tmux ~/.config/tmux
+ln -s ~/.config/.dotfiles/nvim ~/.config/nvim
+cp -r ~/.config/.dotfiles/wal ~/.cache/wal
+sudo ln -s /snap/nvim/current/usr/bin/nvim /bin/editor
+ln -s ~/.config/.dotfiles/bash/.bashrc ~/.bashrc
+ln -s ~/.config/.dotfiles/bash/.bash_aliases ~/.bash_aliases
