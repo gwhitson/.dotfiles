18ac9fcb18e49ae3dc815c8ae2f9b94fcd410902
gavin whitson <rockywhitson4@gmailcom>
2024-03-27 18:17:45 -0500

    update

--- plasma/link_plasma_cfg.sh
+++ plasma/link_plasma_cfg.sh
@@ -3,32 +3,47 @@
 curr="/home/gavin/.config"
 new="/home/gavin/.config/.dotfiles/plasma"
 
-rm "$curr/plasmarc"
-ln -s "$new/plasmarc" "$curr/plasmarc"
+rm "$curr/kactivitymanagerdrc"
+ln -s "$new/kactivitymanagerdrc" "$curr/kactivitymanagerdrc"
 
-rm "$curr/systemsettingsrc"
-ln -s "$new/systemsettingsrc" "$curr/systemsettingsrc"
+rm "$curr/kconf_updaterc"
+ln -s "$new/kconf_updaterc" "$curr/kconf_updaterc"
 
-rm "$curr/plasmashellrc"
-ln -s "$new/plasmashellrc" "$curr/plasmashellrc"
+rm -rf "$curr/KDE"
+ln -s "$new/KDE" "$curr/KDE"
 
-rm "$curr/plasma-org.kde.plasma.desktop-appletsrc"
-ln -s "$new/plasma-org.kde.plasma.desktop-appletsrc" "$curr/plasma-org.kde.plasma.desktop-appletsrc"
+rm "$curr/kded5rc"
+ln -s "$new/kded5rc" "$curr/kded5rc"
 
-rm "$curr/plasma-localerc"
-ln -s "$new/plasma-localerc" "$curr/plasma-localerc"
-
-rm "$curr/kwinrulesrc"
-ln -s "$new/kwinrulesrc" "$curr/kwinrulesrc"
-
-rm "$curr/kwinrc"
-ln -s "$new/kwinrc" "$curr/kwinrc"
-
-rm "$curr/khotkeysrc"
-ln -s "$new/khotkeysrc" "$curr/khotkeysrc"
+rm "$curr/kdeglobals"
+ln -s "$new/kdeglobals" "$curr/kdeglobals"
 
 rm "$curr/kglobalshortcutsrc"
 ln -s "$new/kglobalshortcutsrc" "$curr/kglobalshortcutsrc"
 
+rm "$curr/khotkeysrc"
+ln -s "$new/khotkeysrc" "$curr/khotkeysrc"
+
+rm "$curr/kwinrc"
+ln -s "$new/kwinrc" "$curr/kwinrc"
+
+rm "$curr/kwinrulesrc"
+ln -s "$new/kwinrulesrc" "$curr/kwinrulesrc"
+
+rm "$curr/plasma-localerc"
+ln -s "$new/plasma-localerc" "$curr/plasma-localerc"
+
+rm "$curr/plasma-org.kde.plasma.desktop-appletsrc"
+ln -s "$new/plasma-org.kde.plasma.desktop-appletsrc" "$curr/plasma-org.kde.plasma.desktop-appletsrc"
+
+rm "$curr/plasmarc"
+ln -s "$new/plasmarc" "$curr/plasmarc"
+
+rm "$curr/plasmashellrc"
+ln -s "$new/plasmashellrc" "$curr/plasmashellrc"
+
 rm -rf "$curr/sakura"
 ln -s "$new/sakura" "$curr/sakura"
+
+rm "$curr/systemsettingsrc"
+ln -s "$new/systemsettingsrc" "$curr/systemsettingsrc"
