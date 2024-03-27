608d1adc54d64a452a7e134e482e5f320f8d4b66
gavin whitson <rockywhitson4@gmailcom>
2024-03-27 18:45:36 -0500

    update

--- scripts/relink.sh
+++ scripts/relink.sh
@@ -8,7 +8,7 @@ rm -rf ~/.bash_aliases
 
 ln -s ~/.config/.dotfiles/tmux ~/.config/tmux
 ln -s ~/.config/.dotfiles/nvim ~/.config/nvim
-cp -r ~/.config/.dotfiles/wal ~/.cache/wal
+ln -s ~/.config/.dotfiles/wal ~/.cache/wal
 sudo ln -s /snap/nvim/current/usr/bin/nvim /bin/editor
 ln -s ~/.config/.dotfiles/bash/.bashrc ~/.bashrc
 ln -s ~/.config/.dotfiles/bash/.bash_aliases ~/.bash_aliases
