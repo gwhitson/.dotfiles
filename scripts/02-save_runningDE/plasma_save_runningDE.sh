a57cd78adab81fdd535c48d99b202dfaf0005665
gavin <rockywhitson4@gmail.com>
2024-04-04 13:46:52 -0500

    saved plasma configuration

--- scripts/02-save_runningDE/plasma_save_runningDE.sh
+++ scripts/02-save_runningDE/plasma_save_runningDE.sh
@@ -6,6 +6,6 @@ running="/home/gavin/.config/.dotfiles/running_DE"
 rm -rf $cfgfile
 cp -r $running $cfgfile
 
-git add *
+git add /home/gavin/.config/.dotfiles/*
 git commit -a -m 'saved plasma configuration'
 git push
