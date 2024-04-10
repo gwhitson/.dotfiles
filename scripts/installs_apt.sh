2901cb022bc0d1eea1cc6d43845042bb0e189f1e
gavin whitson <rockywhitson4@gmailcom>
2024-04-10 00:44:23 -0500

    include wmctrl in installs

--- scripts/installs_apt.sh
+++ scripts/installs_apt.sh
@@ -2,6 +2,7 @@
 sudo apt update -y
 sudo apt install neofetch -y
 sudo apt install gcc -y
+sudo apt install wmctrl -y
 sudo apt install git -y
 sudo apt install python3.10-venv -y
 sudo apt install imagemagick -y
