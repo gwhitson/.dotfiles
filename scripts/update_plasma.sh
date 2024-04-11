6a15f19527ee3085de7cea363d7a1a5e59d0fdef
gavin <rockywhitson4@gmail.com>
2024-04-11 15:59:44 -0500

    update

--- scripts/update_plasma.sh
+++ scripts/update_plasma.sh
@@ -6,6 +6,7 @@ bash $scriptsdir/runningDE_clear.sh
 bash $scriptsdir/runningDE_set_plasma.sh
 bash $scriptsdir/runningDE_link_all_to_config.sh
 bash $scriptsdir/relink_nonDE_cfgs.sh
+bash $scriptsdir/installs_apt.sh
 
 sudo apt update -y && sudo apt upgrade -y
 
