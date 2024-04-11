d6fe8b1b35c09d604d78948bcbbd7e284483d84f
gavin <rockywhitson4@gmail.com>
2024-04-11 16:16:41 -0500

    update

--- scripts/relink_nonDE_cfgs.sh
+++ scripts/relink_nonDE_cfgs.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 rm -rf ~/.config/tmux
 rm -rf ~/.config/nvim
-rm -rf ~/.cache/wal
 rm -rf ~/.bashrc
 rm -rf ~/.bash_aliases
 rm -rf ~/.inputrc
 
 ln -s ~/.config/.dotfiles/tmux ~/.config/tmux
 ln -s ~/.config/.dotfiles/nvim ~/.config/nvim
-ln -s ~/.config/.dotfiles/wal ~/.cache/wal
 ln -s ~/.config/.dotfiles/bash/.bashrc ~/.bashrc
 ln -s ~/.config/.dotfiles/bash/.bash_aliases ~/.bash_aliases
 ln -s ~/.config/.dotfiles/bash/.inputrc ~/.inputrc
